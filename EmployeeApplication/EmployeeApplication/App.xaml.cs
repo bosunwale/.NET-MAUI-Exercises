b3843b91986c28cf7dffa0d793d1f009e9a47427
johnthebeloved <johnoluwale1@gmail.com>
2022-10-08 15:29:37 -0400

    Initial Commit

---
+++ EmployeeApplication/EmployeeApplication/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace EmployeeApplication;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ EmployeeApplication/EmployeeApplication/Models/Employee.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeApplication.Models
+{
+    public class Employee
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+
+        public string Image { get; set; }
+    }
+}
